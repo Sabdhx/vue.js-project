b07cee921ab4095680775e7bb8e4f6f8f17f758d
Sabdhx <syedaliabdullahshah5@gmail.com>
2025-03-25 01:06:02 +0500

    small changes

--- src/components/ReactiveComponent.vue
+++ src/components/ReactiveComponent.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { reactive } from "vue";
+import { reactive, ref } from "vue";
 
 const reactiveVariable = reactive({ count: 0 });
 const reactiveVariable2 = reactive(["ALI","Abdullah", "Umar"]);
@@ -8,6 +8,11 @@ reactiveVariable2[0]  = "usman"
 reactiveVariable2[1]  = "amir"
   reactiveVariable2.push("Ali Abdullah Shah Rizvi")
 }
+
+const reactiveVariable3 = reactive([ref("Ali"),ref("shanza"),ref("Abdullah")]);
+
+
+
 </script>
 <template>
   <button class="px-4 bg-green-400 py-2" @click="reactiveVariable.count++">
@@ -18,4 +23,11 @@ reactiveVariable2[1]  = "amir"
 
   <button class="px-4 bg-green-400 py-2" @click=" handleChangesInReactive ">
     hit that shit  </button>
+
+    <h1>
+{{ reactiveVariable3[0] }}
+    </h1>
+
+
+
 </template>
